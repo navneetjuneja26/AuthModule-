2200bc9fb8a29aaf14980a64a381afe13bc62e60
rishabh singhal <rishabhsinghalrishabh@gmail.com>
2019-03-06 19:15:07 +0530

    updated code

--- public/js/index.js
+++ public/js/index.js
@@ -1,115 +1,208 @@
-	 // Get the modal
-	 var modal = document.getElementById('myModal');
+// // Get the modal
 
-	 // Get the button that opens the modal
-	 var btn = document.getElementById("myBtn");
-	
-	 // Get the <span> element that closes the modal
-	 var span = document.getElementsByClassName("close")[0];
-	
-	 // When the user clicks the button, open the modal 
-	 btn.onclick = function () {
-	   modal.style.display = "block";
-	 }
-	
-	 // When the user clicks on <span> (x), close the modal
-	 span.onclick = function () {
-	   modal.style.display = "none";
-	 }
-	
-	 // When the user clicks anywhere outside of the modal, close it
-	 window.onclick = function (event) {
-	   if (event.target == modal) {
-		 modal.style.display = "none";
-	   }
-	 }
-	
-	// Login
-	var btn = $('#btn1');
-	btn.on('click',function(e){
-		console.log(e);
-		e.preventDefault();
-	var email=document.getElementById('email').value;
-	var password=document.getElementById('password').value;
-	var datatosend={
-		"email":email,
-		"password":password
-	};
-	$.ajax({
-		url:'http://localhost:7000/users/forget',
-		datatype: 'Json',
-		data: datatosend,
-		type: 'Post',
-		Success: function(res){
-			alert("Login Successful");
-		} 
-	});
-	console.log(datatosend);
+function show() {
+	console.log("show called");
+	var modal = document.getElementById("myModal");
+
+	// // Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// // Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	btn.onclick = function () {
+		modal.style.display = "block";
+	}
+	span.onclick = function () {
+		modal.style.display = "none";
+	}
+	window.onclick = function (event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+}
+
+var token;
+
+function showOnLogin() {
+	document.getElementById("id1").style.display = "none";
+	document.getElementById("id2").style.display = "none";
+	document.getElementById("id3").style.display = "none";
+	document.getElementById("id4").style.display = "none";
+}
+
+
+
+// Login Ajax
+var btn = $('#btn1');				// id of button login
+btn.on('click', function (e) {
+	e.preventDefault();
+	var email = document.getElementById('email').value;
+	var password = document.getElementById('password').value;
+	if (!email || !password) {
+		console.log("fill all entries");
+		document.getElementById("id1").style.display = "block";
+		document.getElementById("id2").style.display = "none";
+		document.getElementById("id3").style.display = "none";
+		document.getElementById("id4").style.display = "none";
+	}
+	else {
+		var data = {
+			"email": email,
+			"password": password
+		};
+		$.ajax({
+			url: 'http://localhost:7000/users/login',
+			type: 'POST',
+			data: data,
+			dataType: 'json',
+			success: function (res) {
+				if (res.message == "no email found") {
+					// flash message 
+					document.getElementById("id1").style.display = "none";
+					document.getElementById("id2").style.display = "block";
+					document.getElementById("id3").style.display = "none";
+					document.getElementById("id4").style.display = "none";
+					console.log("no email");
+				}
+				if (res.message == "token generated successfully") {
+					// flash message 
+
+					localStorage.setItem("token", res.token);
+					window.location = "http://localhost:7000/check.html";
+				}
+				if (res.message == "id pass donot match") {
+					document.getElementById("id1").style.display = "none";
+					document.getElementById("id2").style.display = "none";
+					document.getElementById("id3").style.display = "block";
+					document.getElementById("id4").style.display = "none";
+					console.log("id pass not match");
+				}
+			}
+		});
+	}
 })
-	
-	// sending mail on clicking forget password
-	var myBtn = $('#myBtn');
-	myBtn.on('click',function(e){
-		console.log(e);
-		e.preventDefault();
-	var email=document.getElementById("email").value;
-	var password=document.getElementById('password').value;
-	console.log(email);
-	var data={
-		"email":email
-	};
-	$.ajax({
-		url:'http://localhost:7000/users/login',
-		datatype: 'Json',
-		data: data,
-		type: 'Post',
-		Success: function(res){
-			alert("Login Successful");
-		} 
-	});
-   
-	});
+
+// sending mail on clicking forget password
+var myBtn = $('#myBtn');
+myBtn.on('click', function (e) {
+	e.preventDefault();
+	var email = document.getElementById("email").value;
+	if (!email) {
+		console.log("fill all entries");
+		document.getElementById("id1").style.display = "none";
+		document.getElementById("id2").style.display = "none";
+		document.getElementById("id3").style.display = "none";
+		document.getElementById("id4").style.display = "block";
+	}
+	else {
+		var data = {
+			"email": email
+		};
+		$.ajax({
+			url: 'http://localhost:7000/users/forget',
+			type: 'POST',
+			data: data,
+			dataType: 'json',
+			success: function (res) {
+				if (res.message == "no email exist") {
+					document.getElementById("id1").style.display = "none";
+					document.getElementById("id2").style.display = "block";
+					document.getElementById("id3").style.display = "none";
+					document.getElementById("id4").style.display = "none";
+				}
+				if (res.message == "mail send") {
+					console.log("mail send");
+					show();
+
+				}
+				if (res.message == "mail not send") {
+					console.log("not send");
+				}
+			}
+		});
+	}
+});
 
 
 // resetting password
-
 var reset = $('#Reset');
-	reset.on('click',function(e){
-		console.log(e);
-		e.preventDefault();
-	var pass=document.getElementById("p1").value;
-	var password=document.getElementById('p2').value;
-	console.log(email);
-	var data={
-		"newpassword":pass,
-		"confirmpassword":password,
-		"email": "2016004771.rishabh@ug.sharda.ac.in"
+reset.on('click', function (e) {
+	console.log(e);
+	e.preventDefault();
+	var pass = document.getElementById("p1").value;
+	var password = document.getElementById('p2').value;
+	if (!pass || !password) {
+		console.log("fill all entries");
+		document.getElementById("r1").style.display = "block";
+		document.getElementById("r2").style.display = "none";
+	}
+	else {
+		console.log("second part");
+		if (pass != password) {
+			console.log("password dont match");
+			document.getElementById("r1").style.display = "none";
+			document.getElementById("r2").style.display = "block";
+		}
+		else {
+			var data = {
+				"newpassword": pass,
+				"confirmpassword": password,
+				"token": token
+			};
+			console.log(data);
+			$.ajax({
+				url: 'http://localhost:7000/users/updatepassword',
+				type: 'POST',
+				data: data,
+				dataType: 'json',
+				success: function (res) {
+					if (res.message == "password has been changed") {
+						window.location = "http://localhost:7000/check.html";
+					}
+					if (res.message == "pasword not match") {
+						document.getElementById("r1").style.display = "none";
+						document.getElementById("r2").style.display = "block";
+					}
+					if (res.message == "fill all entries") {
+						document.getElementById("r1").style.display = "block";
+						document.getElementById("r2").style.display = "none";
+					}
+				}
+			});
+		}
+	}
+})
+
+// function will call when reset page will open to check the session 
+function start() {
+	var getUrlParameter = function getUrlParameter(sParam) {
+		var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+			sURLVariables = sPageURL.split('&'),
+			sParameterName;
+
+		for (var i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
+
+			if (sParameterName[0] === sParam) {
+				return sParameterName[1] === undefined ? true : sParameterName[1];
+			}
+		}
 	};
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "http://localhost:7000/users/forgetpassword",
-		"method": "POST",
-		"headers": {
-		  "content-type": "application/json",
-		  "cache-control": "no-cache"
-		},
-		"processData": false,
-		"data": data
-	  }
-	  
-	  $.ajax(settings).done(function (response) {
-		console.log(response);
-	  });
-	})
-
-
-
-
-
-
-
-
-
-
-
+	console.log(getUrlParameter('token'));
+	token = getUrlParameter('token');
+	$.ajax({
+		url: 'http://localhost:7000/users/forgetpassword/' + token,
+		type: 'GET',
+		dataType: 'json',
+		success: function (res) {
+			console.log(res.message);
+			if (res.message == "Session Expired") {
+				window.location.href = "http://localhost:7000";
+			}
+		}
+	});
+	document.getElementById("r1").style.display = "none";
+	document.getElementById("r2").style.display = "none";
+}
--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,12 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const crypto = require('crypto');
+const config = require('config');
 
-// route for setting new password
-router.post('/forgetpassword', (req, res) => {
+// route for cheking session 
+router.get('/forgetpassword/:token', (req, res) => {
+    var check = req.params.token;
 
-    var { newpassword, confirmpassword, email } = req.body;
-    let err = [];
     var sql = require('mssql');
     var config = {
         user: 'sa',
@@ -21,80 +21,125 @@ router.post('/forgetpassword', (req, res) => {
     sql.connect(config, function (err) {
         if (err) console.log(err);
         var request = new sql.Request();
-        var q1 = "select TokenExpire from auth where Email = '" + email + "'";
+        var q1 = "select TokenExpire from auth where ResetToken = '" + check + "'";
         request.query(q1, function (err, response) {
             if (err) { res.send({ "message": "error has occured" }) }
-            time = response.recordset[0].TokenExpire;
-            var milliseconds = (new Date).getTime();
-            parseInt(time, 10);
-            parseInt(milliseconds, 10);
-            var diff = milliseconds - time;
-            if (diff < 1800000) {
-                res.send({ "message": "Session Expired" });
-            } else {
-                updatePassword();
+            if (response.rowsAffected == 0) { res.send({ "message": "no email" }) } else {
+                console.log(response.recordset);
+                time = response.recordset[0].TokenExpire;
+                var milliseconds = (new Date).getTime();
+                parseInt(time, 10);
+                parseInt(milliseconds, 10);
+                var diff = milliseconds - time;
+                console.log(diff);
+                if (diff > 1800000) {
+                    res.send({ "message": "Session Expired" });
+                } else {
+                    //updatePassword();
+                    res.send({ "message": "you can change password" });
+                }
             }
         });
     })
 
-    // function for update password
-    function updatePassword() {
-        if (!newpassword || !confirmpassword) {
-            res.send({ "message": "fill all entries" });
+
+});
+
+// route for changing password
+router.post('/updatepassword', (req, res) => {
+    var { newpassword, confirmpassword, token } = req.body;
+
+    let err = [];
+    if (!newpassword || !confirmpassword) {
+        res.send({ "message": "fill all entries" });
+    }
+    else {
+        if (newpassword != confirmpassword) {
+            res.send({ "mesaage": "pasword not match" });
         }
         else {
-            if (newpassword != confirmpassword) {
-                res.send({ "mesaage": "pasword not match" });
-            }
-            else {
-                bcrypt.genSalt(10, (err, salt) => {
-                    bcrypt.hash(newpassword, salt, (err, hash) => {
-                        if (err) throw err;
-                        newpassword = hash;
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newpassword, salt, (err, hash) => {
+                    if (err) throw err;
+                    newpassword = hash;
 
-                        var sql = require('mssql');
-                        const pool = new sql.ConnectionPool({
-                            user: 'sa',
-                            password: 'abcd',
-                            server: 'DESKTOP-VOMOOC8',
-                            database: 'auth'
-                        })
+                    var sql = require('mssql');
+                    const pool = new sql.ConnectionPool({
+                        user: 'sa',
+                        password: 'abcd',
+                        server: 'DESKTOP-VOMOOC8',
+                        database: 'auth'
+                    })
 
-                        var conn = pool;
-                        conn.connect().then(function (err) {
-                            if (err) console.log(err);
-                            var request = new sql.Request(conn);
-                            var q1 = "update auth set password = '" + newpassword + "' where email = '" + email + "'";
-                            request.query(q1, function (err, response) {
-                                if (err) { res.send(err); }
-                                res.send({ "mesaage": "password has been changed" });
-                            });
-                        })
+                    var conn = pool;
+                    conn.connect().then(function (err) {
+                        if (err) console.log(err);
+                        var request = new sql.Request(conn);
+                        var q1 = "update auth set password = '" + newpassword + "' where ResetToken = '" + token + "'";
+                        request.query(q1, function (err, response) {
+                            if (err) { res.send(err); }
+                            res.send({ "message": "password has been changed" });
+                        });
+                    })
 
-                    });
                 });
-            }
+            });
         }
     }
-});
+})
 
 // login handle
 router.post('/login', (req, res, next) => {
-    passport.authenticate('local', {
-        successMessage: res.send("done")
-    })(req, res, next)
+    var { email, password } = req.body;
+    var sql = require('mssql');
+    const pool = new sql.ConnectionPool({
+        user: 'sa',
+        password: 'abcd',
+        server: 'DESKTOP-VOMOOC8',
+        database: 'auth'
+    })
+    var conn = pool;
+    conn.connect().then(function (err) {
+        var request = new sql.Request(conn);
+        var q1 = "select password,role from auth where email = '" + email + "'";
+        request.query(q1, function (err, response) {
+            if (err) { console.log(err); }
+            if (response.rowsAffected == 0) { res.send({ "message": "no email found" }) }
+            else {
+                pass = response.recordset[0].password;
+                bcrypt.compare(password, pass, (err, isMatch) => {
+                    if (err) { console.log(err); }
+
+                    if (isMatch) {
+                        console.log("success");
+                        const user = {
+                            Role: response.recordset[0].role,
+                            email: email
+                        }
+                        jwt.sign({ user }, 'secretkey', { expiresIn: '1d' }, (err, token) => {
+                            console.log(token);
+                            var dataToSend = {
+                                "message": "token generated successfully",
+                                "token": token
+                            }
+                            res.send(dataToSend)
+                        });
+                    } else {
+                        res.send({ "message": "id pass donot match" })
+                    }
+                });
+            }
+        })
+    })
 });
 
 // route for sending mail on clicking forget password
 router.post('/forget', (req, res) => {
     var { email } = req.body;
     var token;
-    const user = {
-        email: email
-    }
-    jwt.sign({ user }, 'secretkey', { expiresIn: '1d' }, (err, tokenToStore) => {
-        token = tokenToStore;
-        console.log(token);
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) { console.log(err); }
+        token = buffer.toString('hex');
     });
     var sql = require('mssql');
     const pool = new sql.ConnectionPool({
@@ -113,25 +158,27 @@ router.post('/forget', (req, res) => {
         var q2 = "update auth set ResetToken = '" + token + "', TokenExpire = '" + tokenExpire + "' where Email = '" + email + "'";
         request.query(q1, function (err, response) {
             console.log(response);
-            console.log(response);
+            //console.log(response);
             if (err) { res.send(err); }
             if (response.rowsAffected == 0) { res.send({ "message": "no email exist" }); }
-            request.query(q2, function (err, resp) {
-                if (err) { res.send(err); }
-                else {
-                    const sgMail = require('@sendgrid/mail');
-                    sgMail.setApiKey('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');
-                    const msg = {
-                        to: email,
-                        from: 'rishabhsinghalrishabh@gmail.com',
-                        subject: 'password reset',
-                        text: 'check',
-                        html: `<a href="http://localhost:7000/Reset.html"><h2>Click here to Reset Password</h2></a>`
+            else {
+                request.query(q2, function (err, resp) {
+                    if (err) { res.send(err); }
+                    else {
+                        const sgMail = require('@sendgrid/mail');
+                        sgMail.setApiKey(config.get('apiKey'));
+                        const msg = {
+                            to: email,
+                            from: 'rishabhsinghalrishabh@gmail.com',
+                            subject: 'password reset',
+                            text: 'check',
+                            html: '<a href="http://localhost:7000/Reset.html?token=' + token + '"><h2>Click here to Reset Password</h2></a>'
+                        }
+                        if (sgMail.send(msg)) { res.send({ "message": "mail send" }); }
+                        else { res.send({ "message": "mail not send" }); }
                     }
-                    if (sgMail.send(msg)) { res.send({ "message": "mail send" }); }
-                    else { res.send({ "message": "mail not send" }); }
-                }
-            });
+                });
+            }
         });
     })
 })
